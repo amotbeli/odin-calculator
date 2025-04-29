8092fe390a7bfe6097c921b05cf20b98b131f2ab
amotbeli <171921528+amotbeli@users.noreply.github.com>
2025-04-29 10:19:55 +0000

    Set up basic display functions

--- script.js
+++ script.js
@@ -19,3 +19,18 @@ function operate(operator, num1, num2) {
       return divide(num1, num2);
   }
 }
+
+const numberButtons = document.querySelectorAll('.numbers');
+const displayPanel = document.getElementById('display')
+
+for (let i = 0; i < numberButtons.length; i++) {
+  numberButtons[i].addEventListener('click', (e) => {
+    if (displayPanel.textContent == 0) displayPanel.textContent = '';
+    displayPanel.textContent += e.target.textContent;
+  })
+}
+
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', () => {
+  displayPanel.textContent = 0;
+})
